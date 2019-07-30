1a0dd220cd286e13fc730a957b6f65bd36e857be
Jan Elznic <jan@elznic.com>
2019-07-30 08:42:06 +0200

    Fixed SQL structure data for users

--- db/mysql/install/data.sql
+++ db/mysql/install/data.sql
@@ -1,2 +1,5 @@
 INSERT INTO `ships` (`id`, `name`, `speed`) VALUES
 (1, 'Enterprise D', 'Warp 9');
+
+INSERT INTO `users` (`id`, `username`, `password`, `authority`, `status`) VALUES
+(1, 'admin', '$2a$10$uCzx91FdBhZl6JVygTlMSeDS0xiN5WObLlE3Hny9hFiFXx/HORjGK', 6, 1);
