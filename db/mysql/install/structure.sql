b2b017c2432bf92a0a8bba3ea762682bd6cecd6e
Jan Elznic <jan@elznic.com>
2019-07-30 06:26:58 +0200

    SQL structure - Fixed cols length max to 767 bytes - utf8mb4_unicode_ci using 3 bytes per character

--- db/mysql/install/structure.sql
+++ db/mysql/install/structure.sql
@@ -1,7 +1,7 @@
 CREATE TABLE `shipyard` (
   `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '',
-  `speed` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '',
+  `name` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '',
+  `speed` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '',
   PRIMARY KEY (`id`),
   UNIQUE KEY `name` (`name`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
