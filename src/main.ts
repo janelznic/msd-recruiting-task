b0b193d1165d05a64c1d1332ce0dc9fa780ad205
Jan Elznic <jan@elznic.com>
2019-07-30 09:08:36 +0200

    Added validation pipes

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { ValidationPipe } from './pipes/validation.pipe';
 import CONFIG from './config';
 
 const port = CONFIG.PORT;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe());
 
   // Swagger
   const options = new DocumentBuilder()
